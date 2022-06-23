3d54bc73f48ce70003fef8c6ec0ea2154f53adaf
Ivan <Ivan.Kudryavtsev@protonmail.com>
2022-06-23 12:16:21 +0100

    Damageable Interface

--- Assets/Player/Scripts/ProjectileCollision.cs
+++ Assets/Player/Scripts/ProjectileCollision.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class ProjectileCollision : MonoBehaviour
 {
+
+    private float damage;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         //uhhhh
         if (other.CompareTag("Player")){
             return;
+        }
+        if (other.CompareTag("Enemy"))
+        {
+            var found = other.transform.gameObject.TryGetComponent<IDamageable>(out IDamageable stat);
+            if (found) stat.Damage(damage);
         }
         Debug.Log("Bullet collided with " + other.name);
         Destroy(gameObject, 0f);
